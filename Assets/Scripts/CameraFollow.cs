23a5f75d2f0740ad7337aec845e6e7a5b5bb1458
Alexander <alexanderelzinga94@gmail.com>
2018-08-08 18:00:57 +0200

    Project Setup Start
    First Setup

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject Player;
+
+    private float CameraFollowSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+        if (Player != null) this.gameObject.transform.position = Vector3.Lerp(this.gameObject.transform.position, new Vector3(Player.transform.position.x, 0, Player.transform.position.z), Time.deltaTime * CameraFollowSpeed);
+
+    }
+}
